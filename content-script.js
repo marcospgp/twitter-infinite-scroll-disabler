cb96afea916ebe6fb1e50c61bc81ebf88d6a9b39
Marcos Pereira <marcospereiradev@gmail.com>
2019-09-17 00:38:19 +0100

    Add show more button

--- content-script.js
+++ content-script.js
@@ -1,9 +1,12 @@
 "use strict";
 
+let feedHeight = 2500;
+const showMoreIncrement = 2500;
+
 const getContainer = () => new Promise((resolve) => {
     const x = $('section[aria-labelledby]');
 
-    if (x.length === 0) {
+    if (x.length === 0 || x.find("article").length === 0) {
         setTimeout(() => { resolve(getContainer()); }, 100);
         return;
     }
@@ -15,15 +18,40 @@ const getContainer = () => new Promise((resolve) => {
 
 console.log("I'm alive!");
 
+const showMore = (container, button) => {
+    feedHeight += showMoreIncrement;
+    container.css("max-height", `${feedHeight}px`);
+    button.css("top", `${feedHeight}px`);
+}
+
 const container = getContainer().then(container => {
     console.log("Got container:");
     console.log(container);
+
     container.css({
-        "max-height": "1200px",
-        overflow: "hidden"
+        "max-height": `${feedHeight}px`,
+        overflow: "hidden",
+        "margin-bottom": "200px"
     });
+
+    const button = $(`
+        <div id="tisd-show-more">
+            <button type="button">Show more</button>
+        </div>
+    `);
+    button.css({
+        width: container.width(),
+        top: `${feedHeight}px`
+    });
+    container.prepend(button);
+
+    const bgColor = $("body").css("background-color");
+
+    if (bgColor === "rgb(21, 32, 43)" || bgColor === "rgb(0, 0, 0)") {
+        button.children("button").css("color", "rgb(255, 255, 255)");
+    } else {
+        button.children("button").css("color", "rgb(0, 0, 0)");
+    }
+
+    button.click(() => showMore(container, button));
 });
-
-// Done: limited the size of the tweets container
-// Next: Add some bottom margin and add a button that increases its height
-
